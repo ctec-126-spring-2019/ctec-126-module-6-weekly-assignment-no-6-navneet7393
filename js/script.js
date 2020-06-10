9b926b78e24e7b0e29ff99cffbfdfb0fe609b612
navneet7393 <56058754+navneet7393@users.noreply.github.com>
2020-06-09 20:54:49 -0700

    Ready for grading

--- js/script.js
+++ js/script.js
@@ -6,20 +6,26 @@
 * This includes the code that you write and the code that was provided.
 */
 
+// create class named PlayingCard
 class PlayingCard {
+    // Use the function to creat properties for element, suit, face
     constructor(element, face, suit) {
-        /*
-        Create properties for:
-        - element
-        - suit
-        - face
-        - img (set this to `img/${face}_of_${suit}.png`)
-        - state (set this to 0)
-        */
-
-        // your code goes here (remove this comment once you have added your code)
+        this.element = element
+        this.suit = suit
+        this.face = face
+        // state (set this to 0)
+        this.state = 0
+        // setting img variable
+        this.img = `img/${face}_of_${suit}.png`
 
         this.element.addEventListener('click', () => {
+            if (this.state == 0) {
+                this.element.src = this.img
+                this.state = 1
+            } else if (this.state == 1){
+                this.element.src = 'img/back.jpg'
+                this.state = 0
+            }
             /*
             - The event listener should be for a click event
             - The event listener should have logic to switch out the this.element.src
@@ -31,26 +37,31 @@ class PlayingCard {
         })
     }
 
+    
+    // shows the faces of the card
     showFaces() {
         this.element.src = this.img
     }
 
+    // shows the back of the card
     showBacks() {
-        this.element.src = 'img/back.png'
+        this.element.src = 'img/back.jpg'
     }
 }
 
 function createCardImage() {
-    /*
-    - Create a constant named img and have it create a new img element
-    - Set the src property of the img to 'img/back.png'
-    - return the img
-    */
-
-    // your code goes here (remove this comment once you have added your code)
+    // Create a constant named img and have it create a new img element
+    const img = document.createElement('img')
+    // Set the src property of the img to 'img/back.png'
+    img.src = 'img/back.jpg'
+    // return the img
+    return img
 }
 
 function displayDeck() {
+    deck.forEach(card=>{
+        container.appendChild(card.element)
+    })
     /*
     - Create a loop that iterates through each card in the deck array
     - in the loop, append the card.element to the container
@@ -67,9 +78,13 @@ function shuffleDeck() {
 }
 
 function removeCard() {
+    // if the deck of cards is not = 0
     if (deck.length != 0) {
+        // find the card that was clicked on 
         card = document.querySelector('img')
+        // remove it form the DOM (first image in the array)
         card.remove()
+        // shift the deck of cards
         deck.shift()
         if (deck.length == 0) {
             actions.innerHTML = 'No cards left in the deck. :-('
@@ -83,14 +98,12 @@ function buildDeck() {
 
     suits.forEach(suit => {
         faces.forEach(face => {
-            /*
-            - Call the createdCardImage() function and assign the return img element to a variable named image
-            - Set the id attribute of the image to `${face}_of_${suit}.png`
-            - Use the .push method to push a new PlayingCard object into the deck array
-            - Do the .push and object creation in a single statement
-            */
-
-            // your code goes here (remove this comment once you have added your code)
+            // Calling the createImage function and assigning the return img to const image
+            const image = createCardImage()
+            // Set the id attribute of the image to `${face}_of_${suit}.png`
+            image.setAttribute('id', `${face}_of_${suit}.png`)
+            // Pus the object in the empty array
+            deck.push(new PlayingCard(image, face, suit))
         })
     })
 }
@@ -101,6 +114,7 @@ function clearActions() {
 
 let deck = []
 
+// get the handle of the ID's
 const container = document.querySelector('#container')
 const actions = document.querySelector('#actions')
 const shuffleBtn = document.querySelector('#shuffle')
@@ -119,7 +133,9 @@ shuffleBtn.addEventListener('click', () => {
 
 removeBtn.addEventListener('click', () => {
     actions.innerHTML = 'A card was removed.'
+    // calls the remove card function
     removeCard()
+    // calls clear actions(the message display) and executes in 5 seconds
     setTimeout(clearActions, 5000)
 })
 
